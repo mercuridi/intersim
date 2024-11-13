378f56fe21a2301d0adae4d56664a0bfdcb763e3
mercuridi <kaibarberharris@protonmail.com>
2024-11-13 12:46:13 +0000

    class declarations

--- main.cpp
+++ main.cpp
@@ -9,25 +9,59 @@ class Calendar {
         // later, improve to add granularity to the calendar
         // years is good enough for early development
         int currentYear;
-        void getCurrentDate() {
+        Calendar(int startYear) {
+            currentYear = startYear;
+        }
+        void printCurrentDate() {
             cout << currentYear;
         };
+        void incrementYear() {
+            currentYear++;
+        }
 };
 
 class Director {
     public:
-        void startWar(Region* sideA, Region* sideB) {
-
+        Event dummyEvent() {
+            return Event();
         };
 };
 
 class Ledger {
     public:
-        vector<string> events;
-        void recordEvent(string eventName) {
-            
+        vector<Event> events;
+        void recordEvent(Event event) {
+            Ledger::events.push_back(event);
         };
-}
+        void printLedger() {
+            for (int i = events.size(); i > 0; i--) {
+                cout << Ledger::events[i].summary << "\n";
+            }
+        }
+};
+
+class Event {
+    public:
+        // change in future to use a date representation instead of a year
+        int year;
+        string summary;
+        Event(int year=900) {
+            year = year;
+            summary = "This is a dummy event that occurred in the year %i", year;
+        };
+};
+
+class War: public Event {
+    public:
+        Region attacker;
+        Region defender;
+        Region winner;
+        Region loser;
+        War(Region attacker, Region defender) {
+            this->attacker = attacker;
+            this->defender = defender;
+        };
+};
 
 class Region {
     public:
@@ -35,7 +69,10 @@ class Region {
         // for now, attitude as a singular representation
         // in future, improve logic to handle full attitude matrices
         int attitude;
-
+        Region(string name="No Name Provided", int attitude=0) {
+            this->name = name;
+            this->attitude = attitude;
+        }
 };
 
 int main() {

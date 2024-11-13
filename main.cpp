133ffd1a9342272f0d36e27b25f3c79daaf0cb83
mercuridi <kaibarberharris@protonmail.com>
2024-11-13 13:50:47 +0000

    makefile + vscode run and debug

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <format>
+
 using namespace std;
 // expecting to split out this file to different cpp files for maintainability
 // in the future. early dev, single file is easier to work with
+
+class Event {
+    public:
+        // change in future to use a date representation instead of a year
+        int year;
+        string summary;
+        Event(int year=900) {
+            this->year = year;
+            summary = std::format("This is a dummy event that occurred in the year {}", year);
+        };
+};
 
 class Calendar {
     public:
@@ -35,33 +48,10 @@ class Ledger {
             Ledger::events.push_back(event);
         };
         void printLedger() {
-            for (int i = events.size(); i > 0; i--) {
+            for (int i = 0; i < events.size(); i++) {
                 cout << Ledger::events[i].summary << "\n";
             }
         }
-};
-
-class Event {
-    public:
-        // change in future to use a date representation instead of a year
-        int year;
-        string summary;
-        Event(int year=900) {
-            year = year;
-            summary = "This is a dummy event that occurred in the year %i", year;
-        };
-};
-
-class War: public Event {
-    public:
-        Region attacker;
-        Region defender;
-        Region winner;
-        Region loser;
-        War(Region attacker, Region defender) {
-            this->attacker = attacker;
-            this->defender = defender;
-        };
 };
 
 class Region {
@@ -76,9 +66,28 @@ class Region {
         }
 };
 
+class War: public Event {
+    public:
+        Region attacker;
+        Region defender;
+        Region winner;
+        Region loser;
+        War(Region attacker, Region defender) {
+            this->attacker = attacker;
+            this->defender = defender;
+        };
+};
+
 int main() {
+    cout << __cplusplus;
+    cout << "\nStarting program\n";
     Ledger ledger;
+    cout << "Ledger created\n";
     Event event;
+    cout << "Event created\n";
     ledger.recordEvent(event);
+    cout << "Event recorded in ledger\n";
     ledger.printLedger();
+    cout << "Ending program\n";
+    return 0;
 };
